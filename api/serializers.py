8c7624e085fd9aa8a082832ed844443b85fa4843
Hung Quoc Tran <quochung.064@gmail.com>
2022-05-25 20:01:56 -0500

    update source of article in json response

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,16 @@ from .models import Article, Source
 from rest_framework import serializers
 
 class ArticleSerializer(serializers.ModelSerializer):
+    source = serializers.SerializerMethodField()
     class Meta:
         model = Article
         fields = (
-            'from_source', 'url', 'title', 'thumbnail', 'author', 
+            'source', 'url', 'title', 'thumbnail', 'author', 
             'published_time', 'content', 
         )
+    
+    def get_source(self, article):
+        """
+        Convert source id to source name
+        """
+        return article.from_source.name
