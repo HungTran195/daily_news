264a8366dedd5c152e0de87a345defc15145407e
Hung Quoc Tran <quochung.064@gmail.com>
2022-05-27 22:52:52 -0500

    update DRF to enable browser render

--- daily_news/settings.py
+++ daily_news/settings.py
@@ -135,7 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
 }
 
 django_heroku.settings(locals())
