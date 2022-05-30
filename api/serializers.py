0340fc8f9ae082bdd41065faeef55091fd4f9667
Hung Quoc Tran <quochung.064@gmail.com>
2022-05-29 22:34:17 -0500

    add slug link to api response

--- api/serializers.py
+++ api/serializers.py
@@ -3,11 +3,12 @@ from rest_framework import serializers
 
 class ArticleSerializer(serializers.ModelSerializer):
     source = serializers.SerializerMethodField()
+    article_link = serializers.SerializerMethodField()
     class Meta:
         model = Article
         fields = (
             'source', 'url', 'title', 'thumbnail', 'author', 
-            'published_time', 
+            'published_time', 'article_link' 
         )
     
     def get_source(self, article):
@@ -15,6 +16,12 @@ class ArticleSerializer(serializers.ModelSerializer):
         Convert source id to source name
         """
         return article.from_source.name
+
+    def get_article_link(self, article):
+        """
+        Create path to get article content
+        """
+        return f'{article.id}/{article.slug}'
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     class Meta:
