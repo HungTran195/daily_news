6b3aea120837cb40a5babc0057c5577de170536e
Hung Quoc Tran <quochung.064@gmail.com>
2022-06-04 22:33:06 -0500

    use pagination for respose

--- daily_news/settings.py
+++ daily_news/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 django_heroku.settings(locals())
