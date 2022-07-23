d2852610c237144ada48be4eb868959d90bfc613
Hung Quoc Tran <quochung.064@gmail.com>
2022-07-22 23:49:02 -0500

    add command to update database

---
+++ feed_generator/management/commands/shrink_db.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from api.models import Article
+import datetime
+
+class Command(BaseCommand):
+    help = "update database"
+
+
+    def handle(self, *args, **options):
+        # Remove articles that is more than 1 month old 
+        # to keep the database small enough in free tire!
+        year, week, _ = datetime.date.today().isocalendar()
+        print(Article.objects.filter(published_time__year=year).filter(published_time__week__lt=week-4).delete())
